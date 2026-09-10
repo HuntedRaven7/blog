db65b1215f4183eb685088f5f78b37f0e53225ba
Robin Groves <EdwardGroves2007@proton.me>
2026-09-10 18:48:35 -0400

    feat: graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
 export default defineConfig({
-	site: isGitHubPages ? 'https://huntedraven7.github.io' : 'https://robin.tarxz.zip',
-	base: isGitHubPages ? '/Blog' : '/blog',
+	site: 'https://robin.tarxz.zip',
+	base: '/blog',
 	build: {
 		format: 'file',
 	},
